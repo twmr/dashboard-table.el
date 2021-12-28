8381fa08b1005d6a2128c3958646ee4066addf95
Thomas Hisch <thomas.hisch@ims.co.at>
2021-12-28 21:38:14 +0100

    Extend the example

--- example.el
+++ example.el
@@ -10,8 +10,15 @@
 
 (define-derived-mode example-table-mode dashboard-table-mode "example-table"
   "example-table mode"
-  ;; here the major-mode is already `example-table-mode`
+  ;; use a local keymap here
+  )
 
+;;;###autoload
+(defun example-table ()
+  "Show a dashboard in a new buffer."
+  (interactive)
+  (switch-to-buffer "example-table")
+  (example-table-mode)
   (setq dashboard-table-section-alist
         '(("Section1" . "pars for section1")
           ("Section2" . "pars for section2")))
@@ -26,11 +33,24 @@
   (dashboard-table--refresh))
 
 ;;;###autoload
-(defun example-table ()
+(defun example-table2 ()
   "Show a dashboard in a new buffer."
   (interactive)
-  (switch-to-buffer "example-table")
-  (example-table-mode))
+  (switch-to-buffer "example-table2")
+  (example-table-mode)
+  (setq dashboard-table-section-alist
+        '(("A1" . "pars for sectionA1")
+          ("B2" . "pars for sectionB2")))
+  (setq dashboard-table-columns
+        [("Key" 40)
+         ("Name2" 55)
+         ("Data1" 10)
+         ])
+  (setq dashboard-table-section-name-column 0)
+  (setq dashboard-table-get-section-data-function
+        #'example-table-get-section-data)
+  (dashboard-table--refresh))
+
 
 ;; TODO better would be a function that takes a section alist + a column
 ;; format and that creates a dashboard function, because setting the alist +
